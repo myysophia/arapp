eaa6541f69807229a2488c97162fa9299bca31b7
myysophia <wangxucs123@126.com>
2026-03-06 23:38:56 +0800

    feat: add swiftui design tokens

---
+++ SharedUI/Theme/AppRadius.swift
@@ -0,0 +1,9 @@
+import CoreGraphics
+
+enum AppRadius {
+    static let sm: CGFloat = 10
+    static let md: CGFloat = 14
+    static let lg: CGFloat = 20
+    static let xl: CGFloat = 28
+    static let pill: CGFloat = 999
+}
