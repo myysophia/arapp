30009dc71528242d54cc23cb6bf51b035d638934
myysophia <wangxucs123@126.com>
2026-03-06 23:38:56 +0800

    feat: add swiftui design tokens

---
+++ SharedUI/Theme/AppShadow.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+enum AppShadow {
+    static let cardColor = Color.black.opacity(0.08)
+    static let cardRadius: CGFloat = 18
+    static let cardX: CGFloat = 0
+    static let cardY: CGFloat = 8
+
+    static let floatingColor = Color.black.opacity(0.12)
+    static let floatingRadius: CGFloat = 26
+    static let floatingX: CGFloat = 0
+    static let floatingY: CGFloat = 14
+}
